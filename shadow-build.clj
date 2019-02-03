4809b9cfcb2a33871148b4899767f270a8cf3d61
Jochen Rau <jrau@infinitecloud.com>
2019-02-02 20:39:56 -0500

    Add build watcher and nREPL script

---
+++ shadow-build.clj
@@ -0,0 +1,7 @@
+;; We first start an NREPL server
+(require '[shadow.cljs.devtools.server :as server])
+(server/start!)
+
+;; Then we start the builds and keep watching for changes
+(require '[shadow.cljs.devtools.api :as shadow])
+(shadow/watch :importer)
